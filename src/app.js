de5c4f10642ec7dcd2a0935f141b2c7570d2349b
ifnavneetrajput <navneetrajput404@gmail.com>
2025-01-17 17:14:07 +0530

    Testing

--- src/app.js
+++ src/app.js
@@ -2,18 +2,34 @@ const express = require("express")
 
 const app = express();
 
-app.use("/navneet", (req, res) => {
-  res.send("hello i am from port 7777 navneet");
-});
-app.use("/virat", (req, res) => {
-  res.send("hello i am from port 7777 virat kholi");
-});
+// app.use("/navneet", (req, res) => {
+//   res.send("hello i am from port 7777 navneet");
+// });
+// app.use("/virat", (req, res) => {
+//   res.send("hello i am from port 7777 virat kholi");
+// });
 
 
-app.use('/test',(req, res) => {
-      res.send('hello i am from port 7777')
+// app.use('/test',(req, res) => {
+//       res.send('hello i am from port 7777')
+// })
+
+app.use("/user", (req, res) => {
+  res.send("hahahahaha")
 })
 
+app.get('/user', (req, res) => {
+  res.send("user get api successful" )
+})
+
+app.post("/user", (req, res) => {
+  // write logic to save data to database
+  res.send("user post api succesful")
+})
+
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully")
+})
 app.listen(7777, () => {
   console.log("This port is running on 7777")
 })
