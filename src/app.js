7983b945710b7920211f615243919b8decdd8fb9
ifnavneetrajput <navneetrajput404@gmail.com>
2025-01-17 22:38:56 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,36 +2,21 @@ const express = require("express")
 
 const app = express();
 
-// app.use("/navneet", (req, res) => {
-//   res.send("hello i am from port 7777 navneet");
-// });
-// app.use("/virat", (req, res) => {
-//   res.send("hello i am from port 7777 virat kholi");
-// });
+app.use(
+  "/user",
+  // you can wrap these functions inside array
+  // there will be an error if you respond with multiple response
+  (req, res ,next) => {
+    console.log("1st handler")
+    //  res.send("1st handler")
+    next();
+  },
+  (req, res) => {
+        console.log("2st handler");
+      res.send("2st handler");
+  }
+);
 
-
-// app.use('/test',(req, res) => {
-//       res.send('hello i am from port 7777')
-// })
-
-// app.use("/user/:userId", (req, res) => {
-
-//   res.send("hahahahaha")
-// })
-
-app.get("/user/:userId", (req, res) => {
-    console.log("userId is", req.params);
-  res.send("user get api successful");
-});
-
-app.post("/user", (req, res) => {
-  // write logic to save data to database
-  res.send("user post api succesful")
-})
-
-app.delete("/user", (req, res) => {
-  res.send("user deleted successfully")
-})
 app.listen(7777, () => {
   console.log("This port is running on 7777")
 })
