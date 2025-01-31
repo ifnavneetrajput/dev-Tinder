05648ff18f0305c207dcdb13fbefe64ff4498f1b
ifnavneetrajput <navneetrajput404@gmail.com>
2025-01-31 18:15:51 +0530

    created a user connection api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,30 +1,54 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequestSchema");
 const userRouter = express.Router();
-
+const USER_SAFE_DATA = ["firstName", "lastName", "about", "skills", "photoUrl"];
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
     const connectionRequest = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
-      status:"interested"
-    }).populate("fromUserId",["firstName","lastName"]);
+      status: "interested",
+    }).populate("fromUserId", ["firstName", "lastName"]);
 
     res.json({
       message: "Data fetched sussefully ",
-      data:connectionRequest
+      data: connectionRequest,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
     })
-    
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequest.map((row) => {
+      if (row.fromUserId.toString() === row.toUserId.toString()) {
+        return row.toUserId;
+      }
+      row.fromUserId;
+    });
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
-  catch (err) {
-    res.status(400).send("ERROR :" +err.message)
-  }
-})
+});
 
 module.exports = {
   userRouter,
-}
+};
